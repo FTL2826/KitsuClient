18d94aef69161e7320951113473ec0ead030a7b6
FTL2826 <gimpastrike@gmail.com>
2023-04-07 22:53:23 +0500

    fully change anime page to combine also remove previous versions of anime & manga feeds remove non combine network methods

--- KitsuClient/KitsuClient/Main flow/Anime page/ViewModel/AnimePageViewModelProtocol.swift
+++ KitsuClient/KitsuClient/Main flow/Anime page/ViewModel/AnimePageViewModelProtocol.swift
@@ -9,13 +9,5 @@ import Foundation
 import Combine
 
 protocol AnimePageViewModelProtocol {
-    var isTrendingLoading: CurrentValueSubject<Bool, Never> { get }
-    var isAlltimeLoading: CurrentValueSubject<Bool, Never> { get }
-    var trendingDataSource: PassthroughSubject<[TitleInfo], Never> { get }
-    var alltimeDataSource: PassthroughSubject<[TitleInfo], Never> { get }
-    
-    
-    func fetchTrendingData()
-    func fetchAlltimeData()
-    func fetchNextPage()
+    func transform(input: AnyPublisher<AnimePageViewModel.Input, Never>) -> AnyPublisher<AnimePageViewModel.Output, Never>
 }
--- KitsuClient/KitsuClient/Services/Network service/APIClientProtocol.swift
+++ KitsuClient/KitsuClient/Services/Network service/APIClientProtocol.swift
@@ -9,17 +9,7 @@ import Foundation
 import Combine
 
 protocol APIClientProtocol: AnyObject {
-    
-    func fetch<Request, Response>(_ endpoint: API.Types.Endpoint,
-                                  method: API.Types.Method,
-                                  body: Request?,
-                                  then callback: ((Result<Response, API.Types.Error>) -> ())?
-    ) where Request: Encodable, Response: Decodable
-    
-    func get<Response>(_ endpoint: API.Types.Endpoint,
-                       then callback: ((Result<Response, API.Types.Error>) -> ())?)
-    where Response: Decodable
-    
+  
     func fetchCombine<Request: Encodable, Response: Decodable>(_ endpoint: API.Types.Endpoint,
                                                                method: API.Types.Method,
                                                                body: Request?) -> AnyPublisher<Response, API.Types.Error>
